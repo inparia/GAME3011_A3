22198307c4960c879e35997bc5c7bcc08ef54298
inparia <inparia@live.ca>
2021-03-28 01:57:40 -0400

    Initial commit

---
+++ Assets/Scripts/LockCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCheck : MonoBehaviour
+{
+    public GameManager gameManager;
+    public int currLockNum;
+    public int[] keyCodes;
+    public int winCondition = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyCodes = new int[3];
+        for (int i = 0; i < 3; i++)
+        {
+            keyCodes[i] = Random.Range(0, 10);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currLockNum = gameManager.currLockNum;
+        
+        if(winCondition == 3)
+        {
+            gameManager.gameWin = true;
+        }
+        else
+        {
+            gameManager.gameWin = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/LockScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScript : MonoBehaviour
+{
+    public int LockNum;
+    public int currNumChoose = 0;
+    public bool rightNum = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("down") && GetComponentInParent<LockCheck>().currLockNum == LockNum)
+        {
+            gameObject.transform.Rotate(36.0f, 0.0f, 0.0f, Space.World);
+            currNumChoose++;
+            if(currNumChoose > 9)
+            {
+                currNumChoose = 0;
+            }
+        }
+        if (Input.GetKeyDown("up") && GetComponentInParent<LockCheck>().currLockNum == LockNum)
+        {
+            gameObject.transform.Rotate(-36.0f, 0.0f, 0.0f, Space.World);
+            currNumChoose--;
+            if(currNumChoose < 0)
+            {
+                currNumChoose = 9;
+            }
+        }
+        
+        switch(LockNum)
+        {
+            case 1:
+                if(currNumChoose == GetComponentInParent<LockCheck>().keyCodes[0])
+                {
+                    if(!rightNum)
+                    {
+                        GetComponentInParent<LockCheck>().winCondition++;
+                    }
+                    rightNum = true;
+                }
+                else
+                {
+                    if(rightNum)
+                    {
+                        GetComponentInParent<LockCheck>().winCondition--;
+                    }
+                    rightNum = false;
+                }
+                break;
+            case 2:
+                if (currNumChoose == GetComponentInParent<LockCheck>().keyCodes[1])
+                {
+                    if (!rightNum)
+                    {
+                        GetComponentInParent<LockCheck>().winCondition++;
+                    }
+                    rightNum = true;
+                }
+                else
+                {
+                    if (rightNum)
+                    {
+                        GetComponentInParent<LockCheck>().winCondition--;
+                    }
+                    rightNum = false;
+                }
+                break;
+            case 3:
+                if (currNumChoose == GetComponentInParent<LockCheck>().keyCodes[2])
+                {
+                    if (!rightNum)
+                    {
+                        GetComponentInParent<LockCheck>().winCondition++;
+                    }
+                    rightNum = true;
+                }
+                else
+                {
+                    if (rightNum)
+                    {
+                        GetComponentInParent<LockCheck>().winCondition--;
+                    }
+                    rightNum = false;
+                }
+                break;
+        }
+    }
+}
